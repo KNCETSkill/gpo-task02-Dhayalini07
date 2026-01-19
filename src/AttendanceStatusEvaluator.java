44840d6304d6de4218c2ec131141df99e03abc83
Dhayalini07 <dhaya0711@gmail.com>
2026-01-19 15:46:56 +0530

    Update AttendanceStatusEvaluator.java

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -1,7 +1,19 @@
-import.java.util.scanner
+import.java.util.Scanner;
   public class AttendanceStatusEvaluator{
     public static void main(String args[]){
-      Scanner sc=new scanner(System.in)
-        int num=sc.nextInt()
-        System.out.println("Enter Student Attendance Percentage:")
+      Scanner sc=new scanner(System.in);
+        System.out.println("Enter Student Attendance Percentage:");
+          int num=sc.nextInt()
+        if(num>85){
+          System.out.println("Excellent");
+          }
+            else if(num>60 && num<85){
+            System.out.println("Satisfactory");
+          }
+            else if(num<60){
+            System.out.println("Poor");
+          }
+        sc.close()
+          }
+  }
          
