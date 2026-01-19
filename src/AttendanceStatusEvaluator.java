38ceeefeee043680a66d87f7d1c8077c45f11a61
Dhayalini07 <dhaya0711@gmail.com>
2026-01-19 15:38:07 +0530

    Update AttendanceStatusEvaluator.java

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -1,1 +1,5 @@
-
+import.java.util.scanner
+  public class AttendanceStatusEvaluator{
+    public static void main(String args[]){
+      Scanner sc=new scanner(System.in)
+        System.out.println(
