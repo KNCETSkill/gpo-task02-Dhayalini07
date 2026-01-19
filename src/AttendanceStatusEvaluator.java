a6a00a5fef44344e6cd069b30d91c2f40577e056
Dhayalini07 <dhaya0711@gmail.com>
2026-01-19 15:40:29 +0530

    Correct syntax in AttendanceStatusEvaluator.java
    Fix syntax errors in AttendanceStatusEvaluator.

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -2,4 +2,6 @@ import.java.util.scanner
   public class AttendanceStatusEvaluator{
     public static void main(String args[]){
       Scanner sc=new scanner(System.in)
-        System.out.println()
+        int num=sc.nextInt()
+        System.out.println("Enter Student Attendance Percentage:")
+         
